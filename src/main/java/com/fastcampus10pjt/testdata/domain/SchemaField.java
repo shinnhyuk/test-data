5d2810714117d6956bb71cb6e97dfb733193db21
shinnhyukk <jinxin4u@gmail.com>
2025-07-21 17:29:20 +0900

    #7 - 도메인 클래스를 엔티티로 전환
    각 도메인이 실제로 JPA를 통해
    DB에 접근할 수 있도록
    엔티티 연관 정보를 추가함.

--- src/main/java/com/fastcampus10pjt/testdata/domain/SchemaField.java
+++ src/main/java/com/fastcampus10pjt/testdata/domain/SchemaField.java
@@ -1,20 +1,77 @@
 package com.fastcampus10pjt.testdata.domain;
 
 import com.fastcampus10pjt.testdata.domain.constant.MockDataType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
+/**
+ * 특정 {@link TableSchema}의 단위 필드 정보
+ * 이 필드들이 모여 테이블 스키마를 구성한다.
+ *
+ * @author 김신혁
+ */
 @Getter
-@Setter
-@ToString
-public class SchemaField {
+@ToString(callSuper = true)
+@Entity
+public class SchemaField extends AuditingFields{
 
-    private String fieldName;
-    private MockDataType mockDataType;
-    private Integer fieldOrder;
-    private Integer blackPercent;
-    private String typeOptionJson;
-    private String forceValue;
+    @Id
+    @GeneratedValue (strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
 
+    @Setter
+    @ManyToOne(optional = false)
+    private TableSchema tableSchema;
+
+    @Setter @Column(nullable = false) private String fieldName;
+    @Setter @Column(nullable = false) private MockDataType mockDataType;
+    @Setter @Column(nullable = false) private Integer fieldOrder;
+    @Setter @Column(nullable = false) private Integer blankPercent;
+
+    @Setter private String typeOptionJson;
+    @Setter private String forceValue;
+
+    protected SchemaField() {
+    }
+
+    public SchemaField(String fieldName, MockDataType mockDataType, Integer fieldOrder, Integer blankPercent, String typeOptionJson, String forceValue) {
+        this.fieldName = fieldName;
+        this.mockDataType = mockDataType;
+        this.fieldOrder = fieldOrder;
+        this.blankPercent = blankPercent;
+        this.typeOptionJson = typeOptionJson;
+        this.forceValue = forceValue;
+    }
+
+    public static SchemaField of(String fieldName, MockDataType mockDataType, Integer fieldOrder, Integer blankPercent, String typeOptionJson, String forceValue) {
+        return new SchemaField(fieldName, mockDataType, fieldOrder, blankPercent, typeOptionJson, forceValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof SchemaField that)) return false;
+
+        if (that.getId() == null) {
+            return Objects.equals(this.getTableSchema().getId(), that.getTableSchema().getId()) &&
+                    Objects.equals(this.getFieldName(), that.getFieldName()) &&
+                    Objects.equals(this.getMockDataType(), that.getMockDataType()) &&
+                    Objects.equals(this.getFieldOrder(), that.getFieldOrder()) &&
+                    Objects.equals(this.getBlankPercent(), that.getBlankPercent()) &&
+                    Objects.equals(this.getTypeOptionJson(), that.getTypeOptionJson()) &&
+                    Objects.equals(this.getForceValue(), that.getForceValue());
+        }
+        return Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return Objects.hash(getTableSchema().getId(), getFieldName(), getMockDataType(), getFieldOrder(), getBlankPercent(), getTypeOptionJson(), getForceValue());
+        }
+        return Objects.hash(getId());
+    }
 }
