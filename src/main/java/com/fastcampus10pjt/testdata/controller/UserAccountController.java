8216b86c89ca68b30b50896c579e87385182add6
shinnhyukk <jinxin4u@gmail.com>
2025-07-25 22:22:26 +0900

    #33 - 회원 컨트롤러 이름 오타 수정
    `UserAccountContoller`를 `UserAccountController`로 수정

--- src/main/java/com/fastcampus10pjt/testdata/controller/UserAccountController.java
+++ src/main/java/com/fastcampus10pjt/testdata/controller/UserAccountController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserAccountContoller {
+public class UserAccountController {
 
     @GetMapping("/my-account")
     public String myAccount() {
