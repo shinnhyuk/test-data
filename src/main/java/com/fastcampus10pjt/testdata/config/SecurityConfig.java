60573edc53f5560d6b1fcaa4c5a8aaed8055161a
shinnhyukk <jinxin4u@gmail.com>
2025-08-01 19:31:19 +0900

    #38 - 인증 규칙 설정 + 내정보 구현 + 컨트롤러 테스트에 인증 표현 추가
    인증 규칙은 처음 서비스 취지에 따라
    인증이 불필요한 정적 리소스,
    비로그인 상태에서도 쓸 수 있어야 하는 메인 및
    테이블 스키마 페이지, 다운로드 링크를 오픈하고
    나머지를 인증으로 막음.
    
    내 정보를 인증 정보로부터 보여주도록 구현하고
    관련 인증 기능들의 검증식을 테스트에 추가.

--- src/main/java/com/fastcampus10pjt/testdata/config/SecurityConfig.java
+++ src/main/java/com/fastcampus10pjt/testdata/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.fastcampus10pjt.testdata.config;
 
+import com.fastcampus10pjt.testdata.domain.dto.security.GithubUser;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -9,6 +12,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -16,7 +20,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/", "table-schema", "table-schema/export").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(withDefaults())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .build();
     }
