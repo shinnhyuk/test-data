576a66a0c04c52ab7a5ec0423c27573980207b1b
shinnhyukk <jinxin4u@gmail.com>
2025-08-03 18:05:48 +0900

    #50 - 테이블 스키마 저장 기능: 저장 + 수정 기능으로 업데이트(upsert)
    `upsert` 패턴을 참고하여,
    서비스 로직 하나에서 경우에 따라
    `insert` 혹은 `update`를 판단하여 수행하도록
    비즈니스 로직을 업데이트

--- src/test/java/com/fastcampus10pjt/testdata/controller/TableSchemaControllerTest.java
+++ src/test/java/com/fastcampus10pjt/testdata/controller/TableSchemaControllerTest.java
@@ -104,7 +104,7 @@ class TableSchemaControllerTest {
                         SchemaFieldRequest.of("age", MockDataType.NUMBER, 3, 20, null, null)
                 )
         );
-        willDoNothing().given(tableSchemaService).saveMySchema(request.toDto(githubUser.id()));
+        willDoNothing().given(tableSchemaService).upsertTableSchema(request.toDto(githubUser.id()));
 
         // When & Then
         mvc.perform(
@@ -115,9 +115,8 @@ class TableSchemaControllerTest {
                                 .with(oauth2Login().oauth2User(githubUser))
                 )
                 .andExpect(status().is3xxRedirection())
-                .andExpect(flash().attribute("tableSchemaRequest", request))
-                .andExpect(redirectedUrl("/table-schema"));
-        then(tableSchemaService).should().saveMySchema(request.toDto(githubUser.id()));
+                .andExpect(redirectedUrlTemplate("/table-schema?schemaName={schemaName}", request.schemaName()));
+        then(tableSchemaService).should().upsertTableSchema(request.toDto(githubUser.id()));
     }
 
     @DisplayName("[GET] 내 스키마 목록 조회 (비로그인)")
